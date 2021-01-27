c96441ddc96ff6ead771768ef84d4187efeede4b
ConsultantsIT <pvce@outlook.com>
2021-01-27 03:28:58 -0500

    commit

--- library/src/decorators/common.ts
+++ library/src/decorators/common.ts
@@ -34,8 +34,79 @@ export function test() {
   return function (
     target: any,
     propertyKey: string,
-    descriptor: PropertyDescriptor
+    descriptor: PropertyDescriptor 
   ) {
     console.log('Decorador TEST  --> invocado');
+    // console.log(propertyKey);
+    // console.log(descriptor);
   };
 }
+
+
+export function ini<T extends {new (...arg: any[]): {}} > (constructor: T) {
+  console.log(constructor);
+  return class extends constructor {
+    total = 15;
+    description = "Nueva descripcion";
+  };
+}
+
+export  function frozen(constructor: Function){
+  Object.freeze(constructor);
+  Object.freeze(constructor.prototype);
+}
+
+export function confirm(status: boolean) {
+  return function (
+    target: Object,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor 
+  ) {
+    //console.log(descriptor.value);
+    const original = descriptor.value;
+
+    descriptor.value = function (...args: any[]){
+      return status === true 
+          ? original.apply(this, args)
+          : 0;
+    }
+  };
+}
+
+// export function prefix (label: string) {
+//   return function (target: any, key: string) {
+//     let val = target[key];
+//     const getter = () => {
+//       return val;
+//     };
+
+//     const setter = (next: string) =>{
+//       console.log("actualizacion valor..");
+//       val = ` ${label} ${next}`;
+//     };
+
+//     Object.defineProperty(target, key, {
+//       get: getter,
+//       set: setter,
+//       enumerable: true,
+//       configurable: true,
+//     });
+//   };
+// }
+
+export const prefix01 = (label: string) => (target: any, key: string) => { 
+  let val = target[key];
+  const getter = () => val;
+
+  const setter = (next: string) =>{
+    console.log("actualizacion valor..");
+    val = ` ${label} ${next}`;
+  };
+
+  Object.defineProperty(target, key, {
+    get: getter,
+    set: setter,
+    enumerable: true,
+    configurable: true,
+  });
+};
--- library/src/utils/calc.ts
+++ library/src/utils/calc.ts
@@ -1,12 +1,17 @@
-import { connect, counter, Logger, test } from './../decorators/common';
+import { connect, counter, frozen, ini, Logger, test, confirm, prefix01 } from './../decorators/common';
 
 /** Calculator with basic operations */
+@frozen
+@ini
 @Logger('Hola mundo')
 export class Calculator {
+  @prefix01("-> -> -> ")
+  total = 0; //?
   constructor() {
     // console.log('Calculator has been loaded...');
   }
 
+  @confirm(true)
   /** Sum any quantity of values */
   sum(...values: number[]): number {
     return values.reduce((previous, current) => {
@@ -17,12 +22,23 @@ export class Calculator {
   // (f ∘ g)(x) es equivalente f(g(x)).
   // (connect ∘ counter)(diff) es equivalente connect(counter(diff)).
   @connect()
-  @counter()
+  @counter() 
   @test()
   /** Substract any quantity of values */
   diff(...values: number[]): number {
-    return values.reduce((previous, current) => {
-      return previous - current;
-    });
+    return values.reduce((previous, current) => previous - current); //?
   }
 }
+
+
+const calc = new Calculator();
+console.log(calc.total);
+// //console.log(calc.description);
+
+// for (let key in calc) {
+//   console.log(`Key: ${key}`);
+// }
+
+//class Sc01 extends Calculator{}
+
+const result = calc.sum(1,2,3); //?
